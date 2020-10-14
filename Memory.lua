6d7d1e8d84cf036f637ce1b9de42390bee131e6e
adrianostanley <adrianostanley@gmail.com>
2020-10-14 20:38:21 -0300

    Add ADDON_NAME and ADDON_VERSION properties

--- Memory.lua
+++ Memory.lua
@@ -6,4 +6,11 @@ Fires up the addon.
 local function initializeCore()
 
   MemoryCore = {};
+
+  -- The addon name
+  MemoryCore.ADDON_NAME = "Memory"
+
+  -- The addon version which is the same as the toc file
+  MemoryCore.ADDON_VERSION = "0.1.0-alpha";
+
 end
