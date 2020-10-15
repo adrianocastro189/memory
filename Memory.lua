1a5540d07a78817ba823ca57b82814efe177fbe9
adrianostanley <adrianostanley@gmail.com>
2020-10-14 21:04:45 -0300

    Add a missing semicolon

--- Memory.lua
+++ Memory.lua
@@ -9,7 +9,7 @@ local function initializeCore()
   MemoryCore = {};
 
   -- The addon name
-  MemoryCore.ADDON_NAME = "Memory"
+  MemoryCore.ADDON_NAME = "Memory";
 
   -- The addon version which is the same as the toc file
   MemoryCore.ADDON_VERSION = "0.1.0-alpha";
