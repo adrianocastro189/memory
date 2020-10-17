cd05123e5a6493588034d4f42dfad53149b244ad
adrianostanley <adrianostanley@gmail.com>
2020-10-16 22:51:14 -0300

    Create the MemoryRepository class structure

---
+++ includes/repository/MemoryRepository.lua
@@ -0,0 +1,28 @@
+--[[
+The repository prototype.
+
+Provides all read and write methods to manipulate the player memory.
+
+@since 0.2.0-alpha
+]]
+MemoryRepository = {};
+MemoryRepository.__index = MemoryRepository;
+
+--[[
+Constructs a new instance of a memory repository.
+
+@since 0.2.0-alpha
+
+@param string realm
+@param string player
+@returns MemoryRepository
+]]
+function MemoryRepository:new( player, realm )
+
+  local instance = {};
+  setmetatable(instance, MemoryRepository);
+  instance.player = player;
+  instance.realm = realm;
+
+  return instance;
+end
