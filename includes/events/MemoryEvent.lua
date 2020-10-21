c9bb0fa9697416361b39c0b83db50ac7fbfbd97f
adrianostanley <adrianostanley@gmail.com>
2020-10-21 20:04:14 -0300

    Add the MemoryEvent class structure

---
+++ includes/events/MemoryEvent.lua
@@ -0,0 +1,16 @@
+--[[
+The event handler that captures players actions and stores memories from them.
+
+@since 0.3.0-alpha
+]]
+MemoryEvent = {};
+MemoryEvent.__index = MemoryEvent;
+
+--[[
+Constructs a new instance of a memory event.
+
+@since 0.3.0-alpha
+]]
+function MemoryEvent:new()
+
+end
