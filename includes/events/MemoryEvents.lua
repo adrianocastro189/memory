1327748b01e21114e4c34e0910c9126904c3f440
adrianostanley <adrianostanley@gmail.com>
2020-10-25 16:05:57 -0300

    Add the appendEvents method

---
+++ includes/events/MemoryEvents.lua
@@ -0,0 +1,11 @@
+--[[
+Adds all the available memory event implementations to core as listeners.
+
+@since 0.4.0-alpha
+
+@param MemoryCore core the memory core instance
+]]
+function MemoryAddon_appendEvents( core )
+
+  MemoryCore:debug( "Events appended" );
+end
