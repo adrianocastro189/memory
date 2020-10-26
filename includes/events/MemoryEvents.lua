f05cf24c60883c859de86e2012e436fd299272e9
adrianostanley <adrianostanley@gmail.com>
2020-10-26 20:18:42 -0300

    Add the EventNpcBusiness stub

--- includes/events/MemoryEvents.lua
+++ includes/events/MemoryEvents.lua
@@ -7,5 +7,18 @@ Adds all the available memory event implementations to core as listeners.
 ]]
 function MemoryAddon_appendEvents( core )
 
+  --[[
+  Event triggered when a player buys, sells or repairs with an NPC.
+
+  @since 0.4.0-alpha
+  ]]
+  core:addEventListener( MemoryEvent:new(
+    "EventNpcBusiness",
+    {},
+    function( event, params )
+
+    end
+  ) );
+
   MemoryCore:debug( "Events appended" );
 end
