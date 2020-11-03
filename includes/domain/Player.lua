0e4ee6ae473752740446539cda64f49916c1baa3
adrianostanley <adrianostanley@gmail.com>
2020-11-03 20:05:48 -0300

    Add the get player on target core method

--- includes/domain/Player.lua
+++ includes/domain/Player.lua
@@ -37,6 +37,22 @@ function MemoryAddon_addPlayerPrototype( core )
   end
 
 
+  --[[
+  Gets the player of the current player's target.
+
+  @since 0.4.0-alpha
+
+  @return MemoryAddon_Player
+  ]]
+  function core:getPlayerOnTarget()
+
+    return MemoryAddon_Player:new(
+      UnitGUID( 'target' ) or '', -- gets the target unit ID or use an empty string instead of nil
+      UnitName( 'target' ) or ''  -- gets the target unit name or use an empty string instead of nil
+    );
+  end
+
+
   -- prevents this method to be called again
   MemoryAddon_addPlayerPrototype = nil;
 end
