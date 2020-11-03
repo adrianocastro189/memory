b3baa939bcdfe8d9baa32a9541d2a3a3f5bfbead
adrianostanley <adrianostanley@gmail.com>
2020-11-03 20:05:18 -0300

    Add the player prototype and constructor

--- includes/domain/Player.lua
+++ includes/domain/Player.lua
@@ -7,6 +7,36 @@ Adds the player prototype and creation method to core.
 ]]
 function MemoryAddon_addPlayerPrototype( core )
 
+  --[[
+  The player prototype.
+
+  @since 0.4.0-alpha
+  ]]
+  local MemoryAddon_Player = {};
+  MemoryAddon_Player.__index = MemoryAddon_Player;
+
+
+  --[[
+  Builds a new instance of a player.
+
+  @since 0.4.0-alpha
+
+  @param string guid player's unique ID
+  @param string name player's name
+  @return MemoryAddon_Player
+  ]]
+  function MemoryAddon_Player:new( guid, name )
+
+    local instance = {};
+    setmetatable( instance, MemoryAddon_Player );
+    instance.guid = guid;
+    instance.name = name;
+    instance.type = nil;
+
+    return instance;
+  end
+
+
   -- prevents this method to be called again
   MemoryAddon_addPlayerPrototype = nil;
 end
