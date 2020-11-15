67d28734236350b88e0b4d9a220b5fb1bf633c02
Adriano Castro <adrianostanley@gmail.com>
2020-11-15 15:34:55 -0300

    Add parseChatMsgLoot to CompatibilityHelper

--- includes/helpers/CompatibilityHelper.lua
+++ includes/helpers/CompatibilityHelper.lua
@@ -20,6 +20,11 @@ function MemoryAddon_CompatibilityHelper:new()
   local instance = {};
   setmetatable( instance, MemoryAddon_CompatibilityHelper );
 
+  -- pattern for a single "You receive loot: %s." chat msg loot
+  instance.PATTERN_LOOT_ITEM_SELF = LOOT_ITEM_SELF:gsub( '%%s', '(.+)' );
+
+  -- pattern for multiple "You receive loot: %sx%d." chat msg loot
+  instance.PATTERN_LOOT_ITEM_SELF_MULTIPLE = LOOT_ITEM_SELF_MULTIPLE:gsub( '%%s', '(.+)' ):gsub( '%%d', '(%%d+)' );
 
   --[[
   Gets the dialog gossip title, which will be the npc name for almost all cases.
@@ -116,6 +121,60 @@ function MemoryAddon_CompatibilityHelper:new()
   function instance:isFlying()
 
     return not ( IsFlying == nil ) and IsFlying();
+  end
+
+
+  --[[
+  Gets an item loot information from a loot string got from CHAT_MSG_LOOT payload.
+
+  @since 0.4.0-alpha
+
+  @return mixed item loot information with string name, bool valid and int quantity
+  ]]
+  function instance:parseChatMsgLoot( msg )
+
+    -- sanity check
+    msg = msg or '';
+
+    -- return object with item loot information
+    local item = {};
+
+    -- default quantity (may be replaced only if multiple loot is found)
+    item.quantity = 1;
+
+    -- determines whether the looted item is valid
+    item.valid = true;
+
+    if msg:match( self.PATTERN_LOOT_ITEM_SELF_MULTIPLE ) then
+
+      -- when the player loots more than 1 of the same item
+			itemLink, quantity = string.match( msg, self.PATTERN_LOOT_ITEM_SELF_MULTIPLE );
+
+      item.quantity = quantity;
+
+		elseif msg:match( self.PATTERN_LOOT_ITEM_SELF ) then
+
+      -- when the player loots only 1 item
+			itemLink = string.match( msg, self.PATTERN_LOOT_ITEM_SELF );
+
+		else
+
+      MemoryCore:debug( 'Invalid loot item msg' );
+
+      item.valid = false;
+    end
+
+    if item.valid then
+
+      -- gets the item name from the extracted item link
+      local itemName = GetItemInfo( itemLink );
+
+      -- considering that the item link can be invalid, do a sanity check
+      item.valid = itemName ~= nil and itemName ~= '';
+      item.name  = itemName;
+    end
+
+    return item;
   end
 
 
