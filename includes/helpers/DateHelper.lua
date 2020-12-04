d5f3ac766e7b39ded073dfa2b29ead085ea03ddf
Adriano Castro <adrianostanley@gmail.com>
2020-12-04 20:50:51 -0300

    Add getMonthName method

--- includes/helpers/DateHelper.lua
+++ includes/helpers/DateHelper.lua
@@ -42,6 +42,34 @@ function MemoryAddon_DateHelper:new()
 
     return self:getMonthName( month ) .. ' ' .. tonumber( day ) .. ', ' .. year;
   end
+
+
+  --[[
+  Gets the month name from its index.
+
+  @since 0.6.0-beta
+
+  @param string|int month index from 1 to 12
+  @return string
+  ]]
+  function instance:getMonthName( month )
+
+    month = tonumber( month );
+
+    if 1  == month then return 'January';   end
+    if 2  == month then return 'February';  end
+    if 3  == month then return 'March';     end
+    if 4  == month then return 'April';     end
+    if 5  == month then return 'May';       end
+    if 6  == month then return 'June';      end
+    if 7  == month then return 'July';      end
+    if 8  == month then return 'August';    end
+    if 9  == month then return 'September'; end
+    if 10 == month then return 'October';   end
+    if 11 == month then return 'November';  end
+    if 12 == month then return 'December';  end
+
+    return 'Invalid month';
   end
 
 
