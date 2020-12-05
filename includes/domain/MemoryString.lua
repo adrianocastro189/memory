a9e8138d988a067ec653d3ff9d951017cba3c70a
Adriano Castro <adrianostanley@gmail.com>
2020-12-04 21:32:50 -0300

    Add MemoryString prototype

---
+++ includes/domain/MemoryString.lua
@@ -0,0 +1,50 @@
+--[[
+Adds the memory string prototype and creation method to repository.
+
+@since 0.6.0-beta
+
+@param MemoryRepository repository the memory repository instance
+]]
+function MemoryAddon_addMemoryStringPrototype( repository )
+
+  --[[
+  The memory string prototype.
+
+  @since 0.6.0-beta
+  ]]
+  local MemoryAddon_MemoryString = {};
+  MemoryAddon_MemoryString.__index = MemoryAddon_MemoryString;
+
+  --[[
+  Builds a new instance of a memory string.
+
+  @since 0.6.0-beta
+
+  @return MemoryAddon_MemoryString
+  ]]
+  function MemoryAddon_MemoryString:new()
+
+    local instance = {};
+
+    setmetatable( instance, MemoryAddon_MemoryString );
+
+    return instance;
+  end
+
+
+  --[[
+  Builds a new instance of a memory string from repository.
+
+  @since 0.6.0-beta
+
+  @return MemoryAddon_MemoryString
+  ]]
+  function repository:newMemoryString()
+
+    return MemoryAddon_MemoryString:new();
+  end
+
+
+  -- prevents this method to be called again
+  MemoryAddon_addMemoryStringPrototype = nil;
+end
