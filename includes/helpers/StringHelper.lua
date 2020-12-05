b24413d8655a13ef8b93fb05f984253134f95d98
Adriano Castro <adrianostanley@gmail.com>
2020-12-05 20:35:44 -0300

    Add getOrdinalSuffix method to string helper

--- includes/helpers/StringHelper.lua
+++ includes/helpers/StringHelper.lua
@@ -21,6 +21,29 @@ function MemoryAddon_StringHelper:new()
   setmetatable( instance, MemoryAddon_StringHelper );
 
 
+  --[[
+  Gets the orginal suffix for a number.
+
+  For 1, this method will return st, for 2, nd, and so on.
+
+  @since 0.6.0-alpha
+
+  @param number
+  @return string
+  ]]
+  function instance:getOrdinalSuffix( number )
+
+      local j = number % 10;
+      local k = number % 100;
+
+      if ( j == 1 and k ~= 11 ) then return 'st'; end
+      if ( j == 2 and k ~= 12 ) then return 'nd'; end
+      if ( j == 3 and k ~= 13 ) then return 'rd'; end
+
+      return 'th';
+  end
+
+
   -- destroys the prototype, so instance will be unique
   MemoryAddon_StringHelper = nil;
 
