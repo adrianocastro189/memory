40d3a0c2c87bb78b3bcd6ceab808fd5cff95a472
Adriano Castro <adrianostanley@gmail.com>
2020-12-12 15:27:58 -0300

    Add log level constants and property

--- includes/helpers/LoggerHelper.lua
+++ includes/helpers/LoggerHelper.lua
@@ -20,6 +20,14 @@ function MemoryAddon_LoggerHelper:new()
   local instance = {};
   setmetatable( instance, MemoryAddon_LoggerHelper );
 
+  instance.LEVEL_DEBUG =  0;
+  instance.LEVEL_INFO  = 10;
+  instance.LEVEL_WARN  = 20;
+  instance.LEVEL_ERROR = 30;
+  instance.LEVEL_OFF   = 40;
+
+  -- current log level
+  instance.logLevel = instance.LEVEL_DEBUG;
 
   -- destroys the prototype, so instance will be unique
   MemoryAddon_LoggerHelper = nil;
