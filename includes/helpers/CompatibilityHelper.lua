d228f1356fe84af485b265e429bcce9da8c4e7a2
Adriano Castro <adrianostanley@gmail.com>
2020-12-25 16:01:57 -0300

    Replace getGroupGuids with getGroupFullNames

--- includes/helpers/CompatibilityHelper.lua
+++ includes/helpers/CompatibilityHelper.lua
@@ -59,40 +59,43 @@ function MemoryAddon_CompatibilityHelper:new()
 
 
   --[[
-  Gets all the member guids in a party or raid group.
+  Gets all the member full names in a party or raid group.
+
+  A full name is composed by the player's name, a dash and it's realm name without spaces.
 
   @since 0.4.0-alpha
 
-  @return array list of group member guids
+  @return array list of group member full names
   ]]
-  function instance:getGroupGuids()
+  function instance:getGroupFullNames()
 
     -- gets the number of group members
     local groupSize = GetNumGroupMembers();
 
     -- array to be returned
-    local groupGuids = {};
+    local groupFullNames = {};
 
     -- helper var to iterate over group members
     local counter = 1;
 
     -- pointer to the current member
-    local currentMemberGuid = nil;
+    local currentMemberFullName = nil;
 
     while counter <= groupSize do
 
       -- attempts to get a raid or party member
-      currentMemberGuid = UnitGUID( 'raid' .. counter ) or UnitGUID( 'party' .. counter );
+      currentMemberFullName = self:getNameAndRealm( 'raid' .. counter ) or self:getNameAndRealm( 'party' .. counter );
 
-      if nil ~= currentMemberGuid then
+      if nil ~= currentMemberFullName then
 
-        table.insert( groupGuids, currentMemberGuid );
+        table.insert( groupFullNames, currentMemberFullName );
       end
 
       counter = counter + 1;
     end
 
-    return groupGuids;
+    return groupFullNames;
+  end
   end
 
 
