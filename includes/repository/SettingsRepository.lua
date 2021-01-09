265058c68f1c0d67b4bd7f2950daa1a71e5bef5f
Adriano Castro <adrianostanley@gmail.com>
2021-01-09 15:34:52 -0300

    Add an insert method to SettingsRepository

--- includes/repository/SettingsRepository.lua
+++ includes/repository/SettingsRepository.lua
@@ -20,6 +20,34 @@ function MemoryAddon_SettingsRepository:new()
   local instance = {};
   setmetatable( instance, MemoryAddon_SettingsRepository );
 
+
+  --[[
+  Inserts a setting value.
+
+  @since 1.0.0
+
+  @param string key setting's key using the dot-notation
+  @param string value the value to be set
+  ]]
+  function instance:insert( key, value )
+
+    -- will allow the algorithm to access nested settings
+    settingsAux = MemoryAddon_Settings;
+
+    -- walks through the settings saved variable
+    for part in string.gmatch(key, '[^%.]+') do
+
+      -- creates a new setting path if it doesn't exist yet
+      if nil == settingsAux[ part ] then settingsAux[ part ] = {}; end
+
+      settingsAux = settingsAux[ part ];
+    end
+
+    -- inserts the value in the correct index
+    table.insert( settingsAux, value );
+  end
+
+
   -- destroys the prototype, so instance will be unique
   MemoryAddon_SettingsRepository = nil;
 
