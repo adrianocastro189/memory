18c620219f9283f5773775670ab60594aed2cd1b
Adriano Castro <adrianostanley@gmail.com>
2021-01-11 20:10:43 -0300

    Add the main addon slash command

---
+++ includes/core/SlashCommand.lua
@@ -0,0 +1,10 @@
+--[[
+The main slash command for the Memory Addon.
+
+@since 1.0.0
+]]
+SLASH_MEMORYADDON1 = '/memoryaddon'
+SlashCmdList['MEMORYADDON'] = function( arg )
+
+  MemoryCore:print( arg );
+end
