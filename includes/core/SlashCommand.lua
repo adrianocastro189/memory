c12f7110afc03adfa29f99693b91559358da9dfa
Adriano Castro <adrianostanley@gmail.com>
2021-01-11 20:26:22 -0300

    Slash Command: Update documentation

--- includes/core/SlashCommand.lua
+++ includes/core/SlashCommand.lua
@@ -1,6 +1,15 @@
 --[[
 The main slash command for the Memory Addon.
 
+The slash command structure for /memoryaddon is: command key value, where command
+is the command to be executed by Memory Addon, key is the first argument and value
+is the second argument that will get the command tail, regardless of spaces in it.
+
+Example: /memoryaddon set myname My name is Squareone
+  Command: set
+  Key: myname
+  Value: My name is Squareone
+
 @since 1.0.0
 ]]
 SLASH_MEMORYADDON1 = '/memoryaddon'
