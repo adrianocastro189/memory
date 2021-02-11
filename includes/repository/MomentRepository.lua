d7dca306b537818721f4e14ec537b0d4dd374cd9
Adriano Castro <adrianostanley@gmail.com>
2021-02-11 19:58:02 -0300

    Add the MemoryAddon_MomentRepository prototype

---
+++ includes/repository/MomentRepository.lua
@@ -0,0 +1,27 @@
+--[[
+The moment repository prototype.
+
+Provides all read and write methods to manipulate the player moments.
+
+@since 1.1.0
+]]
+MemoryAddon_MomentRepository = {};
+MemoryAddon_MomentRepository.__index = MemoryAddon_MomentRepository;
+
+--[[
+Constructs a new instance of a moment repository.
+
+@since 1.1.0
+
+@return MemoryAddon_MomentRepository
+]]
+function MemoryAddon_MomentRepository:new()
+
+  local instance = {};
+  setmetatable( instance, MemoryAddon_MomentRepository );
+
+  -- destroys the prototype, so instance will be unique
+  MemoryAddon_MomentRepository = nil;
+
+  return instance;
+end
