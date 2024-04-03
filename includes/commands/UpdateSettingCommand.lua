97f921f6f13d0aca9872d1195359f0a298f7e7b1
Adriano Castro <adrianostanley@gmail.com>
2024-04-03 14:10:34 -0300

    Add the UpdateSettingCommand class

---
+++ includes/commands/UpdateSettingCommand.lua
@@ -0,0 +1,23 @@
+local __ = MemoryCore.__
+local str = __.str
+
+local command = __
+    :new('Command')
+    :setOperation('set')
+    :setDescription("Updates a configuration value")
+    :setCallback(function (key, value)
+        -- sanity check
+        if str:isEmpty(key) or str:isEmpty(value) then
+            MemoryCore:print('Please, provide a key and a value to be set');
+
+            return
+        end
+
+        -- sets the setting value
+        MemoryCore:setting(key, value, true);
+
+        -- confirmation to user
+        MemoryCore:print(key .. ' set to ' .. value);
+    end)
+
+__.commands:add(command)
