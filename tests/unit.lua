aafbdf8aa2af08e088fa1e08cb5349cad6e13d7a
Adriano Castro <adrianostanley@gmail.com>
2024-04-03 10:45:40 -0300

    Add the MemoryCore:addMoment() method

--- tests/unit.lua
+++ tests/unit.lua
@@ -1,22 +1,56 @@
 -- mocks the CreateFrame World of Warcraft API method
+-- @TODO: Move this to a separate file <2024.04.03>
 function CreateFrame(name)
     local mockInstance = {}
     mockInstance.__index = mockInstance
+    function mockInstance:AddMessage() end
     function mockInstance:RegisterEvent() end
     function mockInstance:SetPoint() end
     function mockInstance:SetScript() end
     function mockInstance:SetSize() end
     function mockInstance:SetText() end
+    function mockInstance:UnregisterEvent() end
     return mockInstance
 end
-
+GameTooltip = {}
+function GameTooltip:HookScript() end
 SlashCmdList = {}
+DEFAULT_CHAT_FRAME = CreateFrame('Frame')
+LOOT_ITEM_SELF = 'You receive loot : %s|Hitem :%d :%d :%d :%d|h[%s]|h%s.'
+LOOT_ITEM_SELF_MULTIPLE = 'You receive loot: %sx%d.'
+GetZoneText = function() return 'Stormwind City' end
+GetSubZoneText = function() return 'Trade District' end
+UnitGUID = function(unit) return 'player' end
+UnitLevel = function(unit) return 60 end
+GetRealmName = function() return 'test-realm' end
+date = function(format) return '2024-02-04' end
+-- end of World of Warcraft mocks
 
 lu = require('luaunit')
 
-dofile('./lib/stormwind-library/stormwind-library.lua')
-
-dofile('./Memory.lua')
+--[[
+Loads all addon files in the correct order.
+]]
+local function loadAddonFiles()
+    dofile('./lib/stormwind-library/stormwind-library.lua')
+    dofile('./Memory.lua')
+    dofile('./includes/core/ScreenshotController.lua')
+    dofile('./includes/core/TooltipController.lua')
+    dofile('./includes/domain/Memory.lua')
+    dofile('./includes/domain/MemoryString.lua')
+    dofile('./includes/domain/Player.lua')
+    dofile('./includes/events/MemoryEvent.lua')
+    dofile('./includes/events/MemoryEvents.lua')
+    dofile('./includes/helpers/ArrayHelper.lua')
+    dofile('./includes/helpers/DateHelper.lua')
+    dofile('./includes/helpers/CompatibilityHelper.lua')
+    dofile('./includes/helpers/LoggerHelper.lua')
+    dofile('./includes/helpers/StringHelper.lua')
+    dofile('./includes/repository/MemoryRepository.lua')
+    dofile('./includes/repository/MomentRepository.lua')
+    dofile('./includes/repository/SettingsRepository.lua')
+    dofile('./includes/view/MemoryTextFormatter.lua') 
+end
 
 --[[
 This is a base test class that sets everything up before each test.
@@ -35,7 +69,8 @@ BaseTestClass = {
     end,
     
     setUp = function()
-        -- @TODO: Implement this method once there are objects to be mocked <2024.02.04>
+        loadAddonFiles()
+        MemoryCore.__.events:notify( 'PLAYER_LOGIN' )
     end,
 }
 
