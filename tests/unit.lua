08dfaa50cca882a4bdee1ec9640a7a23b5041c32
Adriano Castro <adrianostanley@gmail.com>
2024-06-14 13:51:29 -0300

    Use wow-mocks.lua instead of the old hardcoded mocks

--- tests/unit.lua
+++ tests/unit.lua
@@ -1,72 +1,10 @@
--- mocks the CreateFrame World of Warcraft API method
--- @TODO: Move this to a separate file <2024.04.03>
-function CreateFrame(name)
-    local mockInstance = {}
-    mockInstance.__index = mockInstance
-    function mockInstance:AddMessage() end
-    function mockInstance:RegisterEvent() end
-    function mockInstance:SetPoint() end
-    function mockInstance:SetScript() end
-    function mockInstance:SetSize() end
-    function mockInstance:SetText() end
-    function mockInstance:UnregisterEvent() end
-    return mockInstance
-end
-GameTooltip = {}
-function GameTooltip:HookScript() end
-SlashCmdList = {}
-DEFAULT_CHAT_FRAME = CreateFrame('Frame')
-LOOT_ITEM_SELF = 'You receive loot : %s|Hitem :%d :%d :%d :%d|h[%s]|h%s.'
-LOOT_ITEM_SELF_MULTIPLE = 'You receive loot: %sx%d.'
-GetZoneText = function() return 'Stormwind City' end
-GetSubZoneText = function() return 'Trade District' end
-UnitGUID = function(unit) return 'player' end
-UnitLevel = function(unit) return 60 end
-date = function(format) return '2024-02-04' end
-
-GetRealmName = function () return 'test-realm' end
-
-UnitGUID = function (unit)
-    if unit == 'player' then
-        return 'test-player-guid'
-    end
-end
-
-UnitName = function (unit)
-    if unit == 'player' then
-        return 'test-player-name'
-    end
-end
--- end of World of Warcraft mocks
-
 lu = require('luaunit')
 
 --[[
 Loads all addon files in the correct order.
 ]]
 local function loadAddonFiles()
-    dofile('./lib/stormwind-library/stormwind-library.lua')
-    dofile('./Memory.lua')
-    dofile('./includes/commands/AddMomentCommand.lua')
-    dofile('./includes/commands/GetMomentCommand.lua')
-    dofile('./includes/commands/GetSettingCommand.lua')
-    dofile('./includes/commands/UpdateSettingCommand.lua')
-    dofile('./includes/core/ScreenshotController.lua')
-    dofile('./includes/core/TooltipController.lua')
-    dofile('./includes/domain/Memory.lua')
-    dofile('./includes/domain/MemoryString.lua')
-    dofile('./includes/domain/Player.lua')
-    dofile('./includes/events/MemoryEvent.lua')
-    dofile('./includes/events/MemoryEvents.lua')
-    dofile('./includes/helpers/ArrayHelper.lua')
-    dofile('./includes/helpers/DateHelper.lua')
-    dofile('./includes/helpers/CompatibilityHelper.lua')
-    dofile('./includes/helpers/LoggerHelper.lua')
-    dofile('./includes/helpers/StringHelper.lua')
-    dofile('./includes/repository/MemoryRepository.lua')
-    dofile('./includes/repository/MomentRepository.lua')
-    dofile('./includes/repository/SettingsRepository.lua')
-    dofile('./includes/view/MemoryTextFormatter.lua') 
+
 end
 
 --[[
@@ -92,19 +30,44 @@ BaseTestClass = {
         -- running this test suite
         _G['TEST_ENVIRONMENT'] = true
 
-        loadAddonFiles()
+        dofile('./tests/wow-mocks.lua')
+
+        dofile('./lib/stormwind-library/stormwind-library.lua')
+
+        dofile('./Memory.lua')
+        dofile('./includes/commands/AddMomentCommand.lua')
+        dofile('./includes/commands/GetMomentCommand.lua')
+        dofile('./includes/commands/GetSettingCommand.lua')
+        dofile('./includes/commands/UpdateSettingCommand.lua')
+        dofile('./includes/core/ScreenshotController.lua')
+        dofile('./includes/core/TooltipController.lua')
+        dofile('./includes/domain/Memory.lua')
+        dofile('./includes/domain/MemoryString.lua')
+        dofile('./includes/domain/Player.lua')
+        dofile('./includes/events/MemoryEvent.lua')
+        dofile('./includes/events/MemoryEvents.lua')
+        dofile('./includes/helpers/ArrayHelper.lua')
+        dofile('./includes/helpers/DateHelper.lua')
+        dofile('./includes/helpers/CompatibilityHelper.lua')
+        dofile('./includes/helpers/LoggerHelper.lua')
+        dofile('./includes/helpers/StringHelper.lua')
+        dofile('./includes/repository/MemoryRepository.lua')
+        dofile('./includes/repository/MomentRepository.lua')
+        dofile('./includes/repository/SettingsRepository.lua')
+        dofile('./includes/view/MemoryTextFormatter.lua') 
+
         MemoryCore.__.events:notify('PLAYER_LOGIN')
+        MemoryCore.__.output:setTestingMode()
     end,
 }
 
-lu.ORDER_ACTUAL_EXPECTED=false
-
 dofile('./tests/MemoryTest.lua')
-
 dofile('./tests/Commands/AddMomentCommandTest.lua')
 dofile('./tests/Commands/GetMomentCommandTest.lua')
 dofile('./tests/Commands/GetSettingCommandTest.lua')
 dofile('./tests/Commands/UpdateSettingCommandTest.lua')
 dofile('./tests/Core/TooltipControllerTest.lua')
+
+lu.ORDER_ACTUAL_EXPECTED=false
 
 os.exit(lu.LuaUnit.run())
