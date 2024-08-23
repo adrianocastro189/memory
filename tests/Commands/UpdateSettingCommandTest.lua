8c14157932dff71498d261d197c4e49a855c116c
Adriano Castro <adrianostanley@gmail.com>
2024-08-23 09:58:21 -0300

    Update TestUpdateSettingCommand to the new TestCase structure

--- tests/Commands/UpdateSettingCommandTest.lua
+++ tests/Commands/UpdateSettingCommandTest.lua
@@ -1,46 +1,62 @@
--- @TODO: Move this test class to the new TestCase structure <2024.07.30>
-
 TestUpdateSettingCommand = BaseTestClass:new()
-    -- @covers includes/commands/UpdateSettingCommand.lua
-    function TestUpdateSettingCommand:testCommandCallback()
-        local keyArg, messageArg, overrideArg, valueArg
 
-        -- mocks
-        function MemoryCore:setting(key, value, override)
-            keyArg = key
-            valueArg = value
-            overrideArg = override
-        end
-        function MemoryCore:print(message) messageArg = message end
+-- @covers includes/commands/UpdateSettingCommand.lua
+TestCase.new()
+    :setName('command callback')
+    :setTestClass(TestUpdateSettingCommand)
+    :setExecution(function()
+        MemoryCore = Spy
+            .new(MemoryCore)
+            :mockMethod('setting')
+            :mockMethod('print')
 
         MemoryCore.commands.operations.set.callback('test-key', 'test-value')
 
-        lu.assertEquals(keyArg, 'test-key')
-        lu.assertEquals(valueArg, 'test-value')
-        lu.assertEquals(overrideArg, true)
-        lu.assertEquals(messageArg, 'test-key set to test-value')
-    end
+        MemoryCore
+            :getMethod('setting')
+            :assertCalledOnceWith('test-key', 'test-value', true)
 
-    -- @covers includes/commands/UpdateSettingCommand.lua
-    function TestUpdateSettingCommand:testCommandCallbackWithEmptyValues()
-        local function execution(key, value)
-            local messageArg
-            
-            function MemoryCore:print(message) messageArg = message end
+        MemoryCore
+            :getMethod('print')
+            :assertCalledOnceWith('test-key set to test-value')
+    end)
+    :register()
 
-            MemoryCore.commands.operations.set.callback(key, value)
+-- @covers includes/commands/UpdateSettingCommand.lua
+TestCase.new()
+    :setName('callback with empty values')
+    :setTestClass(TestUpdateSettingCommand)
+    :setExecution(function(data)
+        MemoryCore = Spy
+            .new(MemoryCore)
+            :mockMethod('print')
 
-            lu.assertEquals('Please, provide a key and a value to be set', messageArg)
-        end
+        MemoryCore.commands.operations.set.callback(data.key, data.value)
 
-        execution(nil, 'test-value')
-        execution('test-key', nil)
-    end
+        lu.assertEquals('Please, provide a key and a value to be set', data.expectedMessage)
+    end)
+    :setScenarios({
+        ['no key provided'] = {
+            key = nil,
+            value = 'test-value',
+            expectedMessage = 'Please, provide a key and a value to be set'
+        },
+        ['no value provided'] = {
+            key = 'test-key',
+            value = nil,
+            expectedMessage = 'Please, provide a key and a value to be set'
+        }
+    })
+    :register()
 
-    -- @covers includes/commands/UpdateSettingCommand.lua
-    function TestUpdateSettingCommand:testCommandWasAdded()
+-- @covers includes/commands/UpdateSettingCommand.lua
+TestCase.new()
+    :setName('command was added')
+    :setTestClass(TestUpdateSettingCommand)
+    :setExecution(function()
         local operations = MemoryCore.commands.operations
 
         lu.assertNotIsNil(MemoryCore.arr:get(operations, 'set'))
-    end
+    end)
+    :register()
 -- end of TestUpdateSettingCommand
