9f2109fff159f3c9c29c4f551acc882beb8fc6db
Adriano Castro <adrianostanley@gmail.com>
2024-08-23 11:31:33 -0300

    Replace MemoryAddon_MemoryString references with MemoryString

--- includes/domain/Memory.lua
+++ includes/domain/Memory.lua
@@ -69,7 +69,7 @@ local Memory = {}
     --[[
     Gets the first time player had this memory.
 
-    @return MemoryAddon_MemoryString
+    @return MemoryString
     ]]
     function Memory:getFirst()
         return self.first
@@ -119,7 +119,7 @@ local Memory = {}
     --[[
     Gets the last time player had this memory.
 
-    @return MemoryAddon_MemoryString
+    @return MemoryString
     ]]
     function Memory:getLast()
         return self.last
@@ -314,7 +314,7 @@ local Memory = {}
     --[[
     Sets the first time player had this memory.
 
-    @param MemoryAddon_MemoryString value
+    @param MemoryString value
 
     @return self
     ]]
@@ -340,7 +340,7 @@ local Memory = {}
     --[[
     Sets the last time player had this memory.
 
-    @param MemoryAddon_MemoryString value
+    @param MemoryString value
 
     @return self
     ]]
