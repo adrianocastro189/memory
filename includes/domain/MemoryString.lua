0335c5023c316e716b9114965657bb72ff22e3d8
Adriano Castro <adrianostanley@gmail.com>
2024-08-23 11:30:37 -0300

    Add test coverage for MemoryString

--- includes/domain/MemoryString.lua
+++ includes/domain/MemoryString.lua
@@ -8,29 +8,29 @@ MemoryString.__index = MemoryString
         local instance = setmetatable({}, MemoryString)
 
         -- default separator between data values in a memory string
-        instance.DATA_SEPARATOR = '|';
+        instance.DATA_SEPARATOR = '|'
 
         -- default value to be used if any information in the memory string is missing
         -- @TODO: Move this to a more broad scope, like a global constant <2024.06.14>
-        instance.DATA_DEFAULT_CHAR = '-';
+        instance.DATA_DEFAULT_CHAR = '-'
 
         -- the date when the memory string was collected
-        instance.date = instance.DATA_DEFAULT_CHAR;
+        instance.date = instance.DATA_DEFAULT_CHAR
 
         -- the moment player was passing through when the memory string was collected
-        instance.moment = instance.DATA_DEFAULT_CHAR;
+        instance.moment = instance.DATA_DEFAULT_CHAR
 
         -- the player's level when the memory string was collected
-        instance.playerLevel = instance.DATA_DEFAULT_CHAR;
+        instance.playerLevel = instance.DATA_DEFAULT_CHAR
 
         -- the subzone where the memory string was collected
-        instance.subZone = instance.DATA_DEFAULT_CHAR;
+        instance.subZone = instance.DATA_DEFAULT_CHAR
 
         -- the zone where the memory string was collected
-        instance.zone = instance.DATA_DEFAULT_CHAR;
+        instance.zone = instance.DATA_DEFAULT_CHAR
 
         -- builds or parses a memory string
-        if nil ~= memoryString then instance:parse(memoryString); else instance:build(); end
+        if nil ~= memoryString then instance:parse(memoryString) else instance:build() end
 
         return instance
     end
@@ -38,270 +38,234 @@ MemoryString.__index = MemoryString
     --[[
     Crafts a memory string used to store important data to a player memory.
 
-    @since 0.6.0-beta
-
-    @return self MemoryAddon_MemoryString
+    @return self
     ]]
     function MemoryString:build()
         -- gets the memory string data values
-        local date        = MemoryCore:getDateHelper():getToday();
-        local moment      = MemoryCore:getMomentRepository():getCurrentMomentIndex();
-        local playerLevel = UnitLevel('player');
-        local zone        = GetZoneText();
-        local subZone     = GetSubZoneText();
+        local date = MemoryCore:getDateHelper():getToday()
+        local moment = MemoryCore:getMomentRepository():getCurrentMomentIndex()
+        local playerLevel = UnitLevel('player')
+        local zone = GetZoneText()
+        local subZone = GetSubZoneText()
 
         -- replaces nil values with a slash
-        if date == nil then date = self.DATA_DEFAULT_CHAR; end
-        if moment == nil then moment = self.DATA_DEFAULT_CHAR; end
-        if playerLevel == nil or playerLevel == 0 then playerLevel = self.DATA_DEFAULT_CHAR; end
-        if zone == nil or zone == '' then zone = self.DATA_DEFAULT_CHAR; end
-        if subZone == nil or subZone == '' then subZone = self.DATA_DEFAULT_CHAR; end
+        if date == nil then date = self.DATA_DEFAULT_CHAR end
+        if moment == nil then moment = self.DATA_DEFAULT_CHAR end
+        if playerLevel == nil or playerLevel == 0 then playerLevel = self.DATA_DEFAULT_CHAR end
+        if zone == nil or zone == '' then zone = self.DATA_DEFAULT_CHAR end
+        if subZone == nil or subZone == '' then subZone = self.DATA_DEFAULT_CHAR end
 
         return self
             :setDate(date)
             :setMoment(moment)
             :setPlayerLevel(playerLevel)
             :setZone(zone)
-            :setSubZone(subZone);
+            :setSubZone(subZone)
     end
 
     --[[
     Gets the date when the memory string was collected.
 
-    @since 0.6.0-beta
-
     @return string
     ]]
     function MemoryString:getDate()
-        return self.date;
+        return self.date
     end
 
     --[[
     Gets the moment when the memory string was collected.
 
-    @since 1.1.0
-
     @param string context will determine the returned moment of this method (index or description)
+
     @return string
     ]]
     function MemoryString:getMoment(context)
         if 'view' == context and self:hasMoment() then
-            return MemoryCore:getMomentRepository():getMoment(tonumber(self.moment));
+            return MemoryCore:getMomentRepository():getMoment(tonumber(self.moment))
         end
 
-        return self.moment;
+        return self.moment
     end
 
     --[[
     Gets the player's level when the memory string was collected.
 
-    @since 0.6.0-beta
-
     @return string
     ]]
     function MemoryString:getPlayerLevel()
-        return self.playerLevel;
+        return self.playerLevel
     end
 
     --[[
     Gets the subzone where the memory string was collected.
 
-    @since 0.6.0-beta
-
     @return string
     ]]
     function MemoryString:getSubZone()
-        return self.subZone;
+        return self.subZone
     end
 
     --[[
     Gets the zone where the memory string was collected.
 
-    @since 0.6.0-beta
-
     @return string
     ]]
     function MemoryString:getZone()
-        return self.zone;
+        return self.zone
     end
 
     --[[
     Determines whether this memory string has a date or not.
 
-    @since 0.6.0-beta
-
     @return bool
     ]]
     function MemoryString:hasDate()
-        return nil ~= self.date and '-1' ~= self.date;
+        return nil ~= self.date and '-1' ~= self.date
     end
 
     --[[
     Determines whether this memory string has a level or not.
 
-    @since 1.1.1
-
     @return bool
     ]]
     function MemoryString:hasLevel()
-        return nil ~= self.playerLevel and self.DATA_DEFAULT_CHAR ~= self.playerLevel;
+        return nil ~= self.playerLevel and self.DATA_DEFAULT_CHAR ~= self.playerLevel
     end
 
     --[[
     Determines whether this memory string has a moment or not.
 
-    @since 1.1.0
-
     @return bool
     ]]
     function MemoryString:hasMoment()
-        return nil ~= self.moment and (tonumber(self.moment) or 0) > 0;
+        return nil ~= self.moment and (tonumber(self.moment) or 0) > 0
     end
 
     --[[
     Determines whether this memory string has a subzone or not.
 
-    @since 1.1.1
-
     @return bool
     ]]
     function MemoryString:hasSubZone()
-        return nil ~= self.subZone and self.DATA_DEFAULT_CHAR ~= self.subZone;
+        return nil ~= self.subZone and self.DATA_DEFAULT_CHAR ~= self.subZone
     end
 
     --[[
     Determines whether this memory string has a zone or not.
 
-    @since 1.1.1
-
     @return bool
     ]]
     function MemoryString:hasZone()
-        return nil ~= self.zone and self.DATA_DEFAULT_CHAR ~= self.zone;
+        return nil ~= self.zone and self.DATA_DEFAULT_CHAR ~= self.zone
     end
 
     --[[
     Gets a location string based on the zone and the subzone this memory string
     was recorded on.
 
-    @since 1.1.1
-
     @return string
     ]]
     function MemoryString:getLocation()
-        local hasZone    = self:hasZone();
-        local hasSubZone = self:hasSubZone() and self:getZone() ~= self:getSubZone();
+        local hasZone = self:hasZone()
+        local hasSubZone = self:hasSubZone() and self:getZone() ~= self:getSubZone()
 
         -- has zone and subzone and they're different
-        if hasZone and hasSubZone then return self:getSubZone() .. ', ' .. self:getZone(); end
+        if hasZone and hasSubZone then return self:getSubZone() .. ', ' .. self:getZone() end
 
         -- has only zone
-        if hasZone then return self:getZone(); end
+        if hasZone then return self:getZone() end
 
         -- has only subzone
-        if hasSubZone then return self:getSubZone(); end
+        if hasSubZone then return self:getSubZone() end
 
         -- has no zone and no subzone
-        return '';
+        return ''
     end
 
     --[[
     Parses a memory string to populate this instance properties.
 
-    @since 0.6.0-beta
-
     @param string memoryString
-    @return self MemoryAddon_MemoryString
+
+    @return self
     ]]
     function MemoryString:parse(memoryString)
         -- explodes the memory string
-        local date, playerLevel, zone, subZone, moment = strsplit(self.DATA_SEPARATOR, memoryString);
+        local date, playerLevel, zone, subZone, moment = MemoryCore.arr:unpack(MemoryCore.str:split(memoryString or '', self.DATA_SEPARATOR))
 
         -- compatibility with older versions of the addon when moment wasn't a thing
-        if moment == nil then moment = self.DATA_DEFAULT_CHAR; end
+        if moment == nil then moment = self.DATA_DEFAULT_CHAR end
 
         return self
             :setDate(date)
             :setMoment(moment)
             :setPlayerLevel(playerLevel)
             :setZone(zone)
-            :setSubZone(subZone);
+            :setSubZone(subZone)
     end
 
     --[[
     Sets the date when the memory string was collected.
 
-    @since 0.6.0-beta
+    @param string value
 
-    @param string date
-    @return self MemoryAddon_MemoryString
+    @return self
     ]]
-    function MemoryString:setDate(date)
-        self.date = date;
-
-        return self;
+    function MemoryString:setDate(value)
+        self.date = value
+        return self
     end
 
     --[[
     Sets the moment when the memory string was collected.
 
-    @since 1.1.0
+    @param string value
 
-    @param string moment
-    @return self MemoryAddon_MemoryString
+    @return self
     ]]
-    function MemoryString:setMoment(moment)
-        self.moment = moment;
-
-        return self;
+    function MemoryString:setMoment(value)
+        self.moment = value
+        return self
     end
 
     --[[
     Sets the player's level when the memory string was collected.
 
-    @since 0.6.0-beta
+    @param string value
 
-    @param string playerLevel
-    @return self MemoryAddon_MemoryString
+    @return self
     ]]
-    function MemoryString:setPlayerLevel(playerLevel)
-        self.playerLevel = playerLevel;
-
-        return self;
+    function MemoryString:setPlayerLevel(value)
+        self.playerLevel = value
+        return self
     end
 
     --[[
     Sets the subzone where the memory string was collected.
 
-    @since 0.6.0-beta
+    @param string value
 
-    @param string subZone
-    @return self MemoryAddon_MemoryString
+    @return self
     ]]
-    function MemoryString:setSubZone(subZone)
-        self.subZone = subZone;
-
-        return self;
+    function MemoryString:setSubZone(value)
+        self.subZone = value
+        return self
     end
 
     --[[
     Sets the zone where the memory string was collected.
 
-    @since 0.6.0-beta
-
     @param string zone
-    @return self MemoryAddon_MemoryString
-    ]]
-    function MemoryString:setZone(zone)
-        self.zone = zone;
 
-        return self;
+    @return self
+    ]]
+    function MemoryString:setZone(value)
+        self.zone = value
+        return self
     end
 
     --[[
     Gets a string representation of the memory string.
-
-    @since 0.6.0-beta
 
     @return string
     ]]
@@ -310,7 +274,7 @@ MemoryString.__index = MemoryString
             '|' .. self:getPlayerLevel() ..
             '|' .. self:getZone() ..
             '|' .. self:getSubZone() ..
-            '|' .. self:getMoment();
+            '|' .. self:getMoment()
     end
 -- End of MemoryString
 
---
+++ tests/Domain/MemoryStringTest.lua
@@ -0,0 +1,398 @@
+TestMemoryString = BaseTestClass:new()
+
+-- @covers MemoryString.__construct()
+TestCase.new()
+    :setName('__construct')
+    :setTestClass(TestMemoryString)
+    :setExecution(function(data)
+        -- can't use spies here
+        local instanceBuildCall = nil
+        local classMock = MemoryCore:getClass('Memory/MemoryString')
+        function classMock:build() instanceBuildCall = 'build' end
+
+        function classMock:parse() instanceBuildCall = 'parse' end
+
+        MemoryCore:addClass('Memory/MemoryString', classMock)
+
+        local instance = MemoryCore:new('Memory/MemoryString', data.memoryStringArg)
+
+        lu.assertNotIsNil(instance)
+        lu.assertEquals('|', instance.DATA_SEPARATOR)
+        lu.assertEquals('-', instance.DATA_DEFAULT_CHAR)
+        lu.assertEquals('-', instance.date)
+        lu.assertEquals('-', instance.moment)
+        lu.assertEquals('-', instance.playerLevel)
+        lu.assertEquals('-', instance.subZone)
+        lu.assertEquals('-', instance.zone)
+
+        lu.assertEquals(data.expectedBuildCall, instanceBuildCall)
+    end)
+    :setScenarios({
+        ['with no memory string'] = {
+            memoryStringArg = nil,
+            expectedBuildCall = 'build'
+        },
+        ['with a memory string'] = {
+            memoryStringArg = '2020-01-01|1|60|Durotar|Valley of Trials',
+            expectedBuildCall = 'parse'
+        }
+    })
+    :register()
+
+-- @covers MemoryString:build()
+TestCase.new()
+    :setName('build')
+    :setTestClass(TestMemoryString)
+    :setExecution(function(data)
+        MemoryCore = Spy
+            .new(MemoryCore)
+            :mockMethod('getDateHelper', function()
+                return {
+                    getToday = function() return data.date end
+                }
+            end)
+            :mockMethod('getMomentRepository', function()
+                return {
+                    getCurrentMomentIndex = function() return data.moment end
+                }
+            end)
+        
+        _G['UnitLevel'] = function() return data.playerLevel end
+        _G['GetZoneText'] = function() return data.zone end
+        _G['GetSubZoneText'] = function() return data.subZone end
+
+        local instance = MemoryCore:new('Memory/MemoryString')
+        
+        instance:build()
+
+        lu.assertEquals(data.expectedDate, instance.date)
+        lu.assertEquals(data.expectedMoment, instance.moment)
+        lu.assertEquals(data.expectedPlayerLevel, instance.playerLevel)
+        lu.assertEquals(data.expectedZone, instance.zone)
+        lu.assertEquals(data.expectedSubZone, instance.subZone)
+    end)
+    :setScenarios({
+        ['with no data values'] = {
+            date = nil,
+            moment = nil,
+            playerLevel = nil,
+            zone = nil,
+            subZone = nil,
+            expectedDate = '-',
+            expectedMoment = '-',
+            expectedPlayerLevel = '-',
+            expectedZone = '-',
+            expectedSubZone = '-'
+        },
+        ['with data values'] = {
+            date = '2020-01-01',
+            moment = 1,
+            playerLevel = 60,
+            zone = 'Durotar',
+            subZone = 'Valley of Trials',
+            expectedDate = '2020-01-01',
+            expectedMoment = 1,
+            expectedPlayerLevel = 60,
+            expectedZone = 'Durotar',
+            expectedSubZone = 'Valley of Trials'
+        },
+    })
+    :register()
+
+-- @covers MemoryString:getLocation()
+TestCase.new()
+    :setName('getLocation')
+    :setTestClass(TestMemoryString)
+    :setExecution(function(data)
+        local instance = MemoryCore:new('Memory/MemoryString')
+
+        instance.zone = data.zone
+        instance.subZone = data.subZone
+
+        lu.assertEquals(data.expectedLocation, instance:getLocation())
+    end)
+    :setScenarios({
+        ['no zone and subzone'] = {
+            zone = '-',
+            subZone = '-',
+            expectedLocation = ''
+        },
+        ['no zone'] = {
+            zone = '-',
+            subZone = 'Valley of Trials',
+            expectedLocation = 'Valley of Trials'
+        },
+        ['no subzone'] = {
+            zone = 'Durotar',
+            subZone = '-',
+            expectedLocation = 'Durotar'
+        },
+        ['zone and subzone'] = {
+            zone = 'Durotar',
+            subZone = 'Valley of Trials',
+            expectedLocation = 'Valley of Trials, Durotar'
+        },
+    })
+    :register()
+
+-- @covers MemoryString:getMoment()
+TestCase.new()
+    :setName('getMoment')
+    :setTestClass(TestMemoryString)
+    :setExecution(function(data)
+        local instance = MemoryCore:new('Memory/MemoryString')
+
+        instance.moment = data.moment
+
+        MemoryCore = Spy
+            .new(MemoryCore)
+            :mockMethod('getMomentRepository', function()
+                return {
+                    getMoment = function() return 'moment' end
+                }
+            end)
+
+        lu.assertEquals(data.expectedMoment, instance:getMoment(data.context))
+    end)
+    :setScenarios({
+        ['view with no moment'] = {
+            moment = '-',
+            context = 'view',
+            expectedMoment = '-'
+        },
+        ['view with moment'] = {
+            moment = 1,
+            context = 'view',
+            expectedMoment = 'moment'
+        },
+        ['no view with moment'] = {
+            moment = 1,
+            context = 'no view',
+            expectedMoment = 1
+        },
+    })
+    :register()
+
+-- @covers MemoryString:hasDate()
+-- @covers MemoryString:hasLevel()
+-- @covers MemoryString:hasMoment()
+TestCase.new()
+    :setName('has methods')
+    :setTestClass(TestMemoryString)
+    :setExecution(function(data)
+        local instance = MemoryCore:new('Memory/MemoryString')
+
+        instance[data.property] = data.value
+
+        lu.assertEquals(data.expectedResult, instance[data.method](instance))
+    end)
+    :setScenarios({
+        ['date with nil'] = {
+            property = 'date',
+            value = nil,
+            method = 'hasDate',
+            expectedResult = false,
+        },
+        ['date with -1'] = {
+            property = 'date',
+            value = '-1',
+            method = 'hasDate',
+            expectedResult = false,
+        },
+        ['database with value'] = {
+            property = 'date',
+            value = '2020-01-01',
+            method = 'hasDate',
+            expectedResult = true,
+        },
+        ['level with nil'] = {
+            property = 'playerLevel',
+            value = nil,
+            method = 'hasLevel',
+            expectedResult = false,
+        },
+        ['level with -'] = {
+            property = 'playerLevel',
+            value = '-',
+            method = 'hasLevel',
+            expectedResult = false,
+        },
+        ['level with value'] = {
+            property = 'playerLevel',
+            value = 60,
+            method = 'hasLevel',
+            expectedResult = true,
+        },
+        ['moment with nil'] = {
+            property = 'moment',
+            value = nil,
+            method = 'hasMoment',
+            expectedResult = false,
+        },
+        ['moment with -'] = {
+            property = 'moment',
+            value = '-',
+            method = 'hasMoment',
+            expectedResult = false,
+        },
+        ['moment with zero'] = {
+            property = 'moment',
+            value = 0,
+            method = 'hasMoment',
+            expectedResult = false,
+        },
+        ['moment with negative number'] = {
+            property = 'moment',
+            value = -1,
+            method = 'hasMoment',
+            expectedResult = false,
+        },
+        ['moment with value'] = {
+            property = 'moment',
+            value = 1,
+            method = 'hasMoment',
+            expectedResult = true,
+        },
+        ['subzone with nil'] = {
+            property = 'subZone',
+            value = nil,
+            method = 'hasSubZone',
+            expectedResult = false,
+        },
+        ['subzone with -'] = {
+            property = 'subZone',
+            value = '-',
+            method = 'hasSubZone',
+            expectedResult = false,
+        },
+        ['subzone with value'] = {
+            property = 'subZone',
+            value = 'Valley of Trials',
+            method = 'hasSubZone',
+            expectedResult = true,
+        },
+        ['zone with nil'] = {
+            property = 'zone',
+            value = nil,
+            method = 'hasZone',
+            expectedResult = false,
+        },
+        ['zone with -'] = {
+            property = 'zone',
+            value = '-',
+            method = 'hasZone',
+            expectedResult = false,
+        },
+        ['zone with value'] = {
+            property = 'zone',
+            value = 'Durotar',
+            method = 'hasZone',
+            expectedResult = true,
+        },
+    })
+    :register()
+
+-- @covers MemoryString:parse()
+TestCase.new()
+    :setName('parse')
+    :setTestClass(TestMemoryString)
+    :setExecution(function(data)
+        local instance = MemoryCore:new('Memory/MemoryString')
+
+        instance:parse(data.memoryString)
+
+        lu.assertEquals(data.expectedDate, instance.date)
+        lu.assertEquals(data.expectedMoment, instance.moment)
+        lu.assertEquals(data.expectedPlayerLevel, instance.playerLevel)
+        lu.assertEquals(data.expectedZone, instance.zone)
+        lu.assertEquals(data.expectedSubZone, instance.subZone)
+    end)
+    :setScenarios({
+        ['no memory string'] = {
+            memoryString = nil,
+            expectedDate = nil,
+            expectedMoment = '-',
+            expectedPlayerLevel = nil,
+            expectedZone = nil,
+            expectedSubZone = nil,
+        },
+        ['with memory string'] = {
+            memoryString = '2020-01-01|60|Durotar|Valley of Trials|1',
+            expectedDate = '2020-01-01',
+            expectedMoment = '1',
+            expectedPlayerLevel = '60',
+            expectedZone = 'Durotar',
+            expectedSubZone = 'Valley of Trials',
+        },
+        ['no moment'] = {
+            memoryString = '2020-01-01|60|Durotar|Valley of Trials',
+            expectedDate = '2020-01-01',
+            expectedMoment = '-',
+            expectedPlayerLevel = '60',
+            expectedZone = 'Durotar',
+            expectedSubZone = 'Valley of Trials',
+        },
+    })
+    :register()
+
+-- @covers MemoryString:getDate()
+-- @covers MemoryString:getMoment()
+-- @covers MemoryString:getPlayerLevel()
+-- @covers MemoryString:getSubZone()
+-- @covers MemoryString:getZone()
+-- @covers MemoryString:setDate()
+-- @covers MemoryString:setMoment()
+-- @covers MemoryString:setPlayerLevel()
+-- @covers MemoryString:setSubZone()
+-- @covers MemoryString:setZone()
+TestCase.new()
+    :setName('setters and getters')
+    :setTestClass(TestMemoryString)
+    :setExecution(function(data)
+        local instance = MemoryCore:new('Memory/MemoryString')
+
+        instance[data.setter](instance, 'value')
+
+        lu.assertEquals('value', instance[data.getter](instance))
+    end)
+    :setScenarios({
+        ['date'] = {
+            setter = 'setDate',
+            getter = 'getDate'
+        },
+        ['moment'] = {
+            setter = 'setMoment',
+            getter = 'getMoment'
+        },
+        ['playerLevel'] = {
+            setter = 'setPlayerLevel',
+            getter = 'getPlayerLevel'
+        },
+        ['zone'] = {
+            setter = 'setZone',
+            getter = 'getZone'
+        },
+        ['subZone'] = {
+            setter = 'setSubZone',
+            getter = 'getSubZone'
+        }
+    })
+    :register()
+
+-- @covers MemoryString:tostring()
+TestCase.new()
+    :setName('tostring')
+    :setTestClass(TestMemoryString)
+    :setExecution(function()
+        local instance = MemoryCore:new('Memory/MemoryString')
+
+        instance.date = '2020-01-01'
+        instance.moment = 1
+        instance.playerLevel = 60
+        instance.zone = 'Durotar'
+        instance.subZone = 'Valley of Trials'
+
+        lu.assertEquals('2020-01-01|60|Durotar|Valley of Trials|1', instance:toString())
+    end)
+    :register()
+-- End of TestMemoryString
